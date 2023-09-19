1f2f16567cc45b364693ef3e08376960982ce9a1
Stanislaw_Khmilovskiy <xmilovskuj@gmail.com>
2023-09-19 18:28:57 +0300

    Completed LAB1

---
+++ LAB1/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Lab1KhmilovskiyKI306","l":"Lab1KhmilovskiyKI306()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab1KhmilovskiyKI306","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
